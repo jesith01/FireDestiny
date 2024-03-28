9cd176c9efbe90c9eeda5a015a6e0514b37ae773
jesith01 <165235235+jesith01@users.noreply.github.com>
2024-03-28 01:28:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,14 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const audioPlayer = document.getElementById('audioPlayer');
-    const playPauseBtn = document.getElementById('playPauseBtn');
-    const songTitle = document.getElementById('songTitle');
-    const progressBarFill = document.querySelector('.progress-bar-fill'); // Seleccionar la barra de progreso que se llena
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play");
 
-    let isPlaying = false;
-
-    playPauseBtn.addEventListener('click', () => {
-        if (isPlaying) {
-            pauseSong();
-        } else {
-            playSong();
-        }
-    });
-
-    function playSong() {
-        audioPlayer.play();
-        playPauseBtn.textContent = 'Pause';
-        isPlaying = true;
-        document.querySelector('.cover-container').classList.add('pulse');
-    }
-
-    function pauseSong() {
-        audioPlayer.pause();
-        playPauseBtn.textContent = 'Play';
-        isPlaying = false;
-        document.querySelector('.cover-container').classList.remove('pulse');
-    }
-
-    audioPlayer.addEventListener('loadedmetadata', () => {
-        const totalTime = formatTime(audioPlayer.duration);
-        document.querySelector('.time-display').textContent = `0:00 / ${totalTime}`;
-    });
-
-    audioPlayer.addEventListener('timeupdate', () => {
-        const currentTime = audioPlayer.currentTime;
-        const totalTime = audioPlayer.duration;
-        const percentage = (currentTime / totalTime) * 100;
-        progressBarFill.style.width = `${percentage}%`; // Actualizar el ancho de la barra de progreso
-        document.querySelector('.time-display').textContent = `${formatTime(currentTime)} / ${formatTime(totalTime)}`;
-    });
-
-    function formatTime(seconds) {
-        const minutes = Math.floor(seconds / 60);
-        const remainderSeconds = Math.floor(seconds % 60);
-        const displaySeconds = remainderSeconds < 10 ? `0${remainderSeconds}` : remainderSeconds;
-        return `${minutes}:${displaySeconds}`;
-    }
+playPause.addEventListener("click", () => {
+  if (audio.paused || audio.ended) {
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+    audio.play();
+  } else {
+    audio.pause();
+    playPause.querySelector(".pause-btn").classList.toggle("hide");
+    playPause.querySelector(".play-btn").classList.toggle("hide");
+  }
 });
